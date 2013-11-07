d0708011a74c96619c779cc0c919d3cd412fbd4c
RichyHBM <git@richyhbm.co.uk>
2013-11-07 22:38:02 +0000

    Finished MatrixManager

--- Wrench/Wrench/src/Managers/MatrixManager.cs
+++ Wrench/Wrench/src/Managers/MatrixManager.cs
@@ -10,20 +10,55 @@ namespace Wrench.src.Managers
     {
         int width = 1, height = 1;
         float nearPlane = 0.1f, farPlane = 100.0f;
+        float fieldOfView = 75;
+        Vector3 position = Vector3.UnitZ, lookAt = Vector3.Zero, up = Vector3.Up;
         Matrix orthogonal;
         Matrix perspective;
+        Matrix view;
+        Matrix viewPerspective;
+        Matrix viewOrthographic;
 
         public MatrixManager()
         {
-            Matrix.CreateOrthographic(width, height, nearPlane, farPlane, out orthogonal);
-            
+            orthogonal = Matrix.CreateOrthographic(width, height, nearPlane, farPlane);
+            view = Matrix.CreateLookAt(position, lookAt, up);
+            perspective = Matrix.CreatePerspectiveFieldOfView(fieldOfView, width / (float)height, nearPlane, farPlane);
+            Matrix.Multiply(ref view, ref orthogonal, out viewOrthographic);
+            Matrix.Multiply(ref view, ref perspective, out viewPerspective);
         }
 
         public void SetWidthHeight(int width, int height)
         {
             this.width = width;
             this.height = height;
-            Matrix.CreateOrthographic(width, height, nearPlane, farPlane, out orthogonal);
+            orthogonal = Matrix.CreateOrthographic(width, height, nearPlane, farPlane);
+            perspective = Matrix.CreatePerspectiveFieldOfView(fieldOfView, width / (float)height, nearPlane, farPlane);
+            Matrix.Multiply(ref view, ref orthogonal, out viewOrthographic);
+            Matrix.Multiply(ref view, ref perspective, out viewPerspective);
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            this.position = position;
+            view = Matrix.CreateLookAt(position, lookAt, up);
+            Matrix.Multiply(ref view, ref orthogonal, out viewOrthographic);
+            Matrix.Multiply(ref view, ref perspective, out viewPerspective);
+        }
+
+        public void SetLookAt(Vector3 lookAt)
+        {
+            this.lookAt = lookAt;
+            view = Matrix.CreateLookAt(position, lookAt, up);
+            Matrix.Multiply(ref view, ref orthogonal, out viewOrthographic);
+            Matrix.Multiply(ref view, ref perspective, out viewPerspective);
+        }
+
+        public void SetUp(Vector3 up)
+        {
+            this.up = up;
+            view = Matrix.CreateLookAt(position, lookAt, up);
+            Matrix.Multiply(ref view, ref orthogonal, out viewOrthographic);
+            Matrix.Multiply(ref view, ref perspective, out viewPerspective);
         }
 
         public Matrix Orthographic
@@ -35,6 +70,24 @@ namespace Wrench.src.Managers
         public Matrix Perspective
         {
             get { return perspective; }
+            private set;
+        }
+
+        public Matrix View
+        {
+            get { return view; }
+            private set;
+        }
+
+        public Matrix ViewPerspective
+        {
+            get { return viewPerspective; }
+            private set;
+        }
+
+        public Matrix ViewOrthographic
+        {
+            get { return viewOrthographic; }
             private set;
         }
 
